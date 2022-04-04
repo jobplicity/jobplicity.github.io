464b42d9a7f4f5d1c2ef3af812af6c0e75c47aab
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2022-04-04 11:00:52 +0700

    update earn and learn padding

--- assets/js/components/earn-learn.js
+++ assets/js/components/earn-learn.js
@@ -2,12 +2,14 @@ export const EarnLearn = {
   template: `
     <section class="earn-learn">
       <div class="container">
-        <div class="row">      
-              <h2>Earn & Learn</h2> 
-              <p>
-                Tired of your student debt growing like there's no tomorrow? We've got you covered. You can finally GET PAID for doing online courses! Sign up to JobLaunch and complete online courses with people around the world and unlock paid micro-jobs which you can add to your resume.    
-              </p>
-              <p>Create your free account today!</p>
+        <div class="row">   
+              <div class="col-md-8 col-xs-10 mx-auto">
+                <h2>Earn & Learn</h2> 
+                <p>
+                  Tired of your student debt growing like there's no tomorrow? We've got you covered. You can finally GET PAID for doing online courses! Sign up to JobLaunch and complete online courses with people around the world and unlock paid micro-jobs which you can add to your resume.    
+                </p>
+                <p>Create your free account today!</p>
+              </div>   
         </div>
       </div>
     </section>
