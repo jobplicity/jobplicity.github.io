685b8d0484b10e78a28cc27c1029bf1d5e178819
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2022-04-04 11:56:09 +0700

    upload microsoft logo

--- assets/js/components/as-used-by.js
+++ assets/js/components/as-used-by.js
@@ -3,14 +3,22 @@ export const AsUsedBy = {
     <section class="as-used-by">
       <div class="container">
         <div class="row">  
-          <p class="text-center">As used by</p>
-          <div class="col d-flex justify-content-center">
+          <div class="col-md-6 col-sm-10 mx-auto justify-content-around">
+            <p class="text-center">used by</p>
+              <div class="row">
+                <div class="col-sm-7">
+                  <img 
+                    src="assets/img/logos/Reactor-Dark.png"
+                  >  
+                </div>
+                <div class="col-sm-5">
+                  <img 
+                    src="assets/img/logos/Microsoft-Dark.png"
+                  > 
+                </div>
+              </div>
 
-            <img 
-              src="assets/img/logos/Reactor-Dark.png"
-            >  
           </div>
-
         </div>
       </div>
     </section>
--- assets/js/components/earn-learn.js
+++ assets/js/components/earn-learn.js
@@ -3,7 +3,7 @@ export const EarnLearn = {
     <section class="earn-learn">
       <div class="container">
         <div class="row">   
-              <div class="col-md-8 col-xs-10 mx-auto">
+              <div class="col-md-8 col-sm-10 mx-auto">
                 <h2>Earn & Learn</h2> 
                 <p>
                   Tired of your student debt growing like there's no tomorrow? We've got you covered. You can finally GET PAID for doing online courses! Sign up to JobLaunch and complete online courses with people around the world and unlock paid micro-jobs which you can add to your resume.    
